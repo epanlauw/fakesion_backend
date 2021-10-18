288f22f240d6e3a65ae65da7cb790d5d0559ce53
epanlauw <epanlauw6@gmail.com>
2021-10-18 12:02:50 +0700

    CORS Problem

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 const methodOverride = require("method-override");
 const session = require("express-session");
 const flash = require("connect-flash");
@@ -37,6 +38,7 @@ app.use(
   })
 );
 app.use(flash());
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
